be5a2127411e22566f06ab10f433ab4932b0c45b
Marco Castro <marcodotcastro@gmail.com>
2017-01-25 14:57:51 -0200

    Preparado para digitalocean

--- app/run.sh
+++ app/run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #
 
-PRIVATE_TOKEN=`curl http://138.197.75.87/api/v3/session --data 'login=root&password=abc12345' | jq -r '.private_token'` && curl -H "Content-Type:application/json" http://138.197.75.87/api/v3/projects?private_token=$PRIVATE_TOKEN -d "{ \"name\": \"sample\" }"
+PRIVATE_TOKEN=`curl http://138.197.75.87:10080/api/v3/session --data 'login=root&password=abc12345' | jq -r '.private_token'` && curl -H "Content-Type:application/json" http://138.197.75.87:10080/api/v3/projects?private_token=$PRIVATE_TOKEN -d "{ \"name\": \"sample\" }"
 
-git clone http://138.197.75.87/root/sample.git
+git clone http://138.197.75.87:10080/root/sample.git
 cd sample
 cp -R ../my_sample/* .
 git add .
